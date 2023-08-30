169895c8176ffc3144b19d9a29df1132cb3062fc
SemjanivNikola <nikola.sem450@gmail.com>
2023-08-30 13:53:28 +0200

    Add Sector + PowerPlant models

---
+++ lib/src/main/java/com/solar/edge/models/PowerPlant.java
@@ -0,0 +1,26 @@
+package com.solar.edge.models;
+
+import java.util.ArrayList;
+
+public class PowerPlant {
+	private int controllerAddress;
+	private ArrayList<Sector> sectorList;
+	
+	public PowerPlant(final int address) {
+		this.controllerAddress = address;
+		this.sectorList = new ArrayList<Sector>();
+	}
+	
+	public void addSector(Sector sector) {
+		this.sectorList.add(sector);
+	}
+	
+	public ArrayList<Sector> getSectorList() {
+		return this.sectorList;
+	}
+	
+	public int getControllerAddress() {
+		return this.controllerAddress;
+	}
+
+}
---
+++ lib/src/main/java/com/solar/edge/models/Sector.java
@@ -0,0 +1,20 @@
+package com.solar.edge.models;
+
+public class Sector {
+	private int controllerAddress;
+	private short numOfPanels;
+	private SolarPanelModel panelModel;
+	private float illuminance;
+	private float voltage;
+	
+	public Sector(final short numOfPanels, final SolarPanelModel model, final int address) {
+		this.numOfPanels = numOfPanels;
+		this.panelModel = model;
+		this.controllerAddress = address;
+	}
+	
+	public int getControllerAddress() {
+		return this.controllerAddress;
+	}
+	
+}
